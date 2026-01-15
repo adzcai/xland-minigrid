ff9c44d861cde25ddac08b79a4db93033054a637
Alexander Cai <adzcai@g.harvard.edu>
2026-01-15 12:06:44 -0500

    add prev action and goal observation wrappers

--- src/xminigrid/wrappers.py
+++ src/xminigrid/wrappers.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from typing import Any, Union
 
 import jax
+import jax.numpy as jnp
 
+from .core.goals import MAX_GOAL_ENCODING_LEN
 from .environment import Environment, EnvParamsT
 from .types import EnvCarryT, IntOrArray, State, TimeStep
 
@@ -79,32 +81,13 @@ class DmEnvAutoResetWrapper(Wrapper):
 class DirectionObservationWrapper(Wrapper):
     def observation_shape(self, params):
         base_shape = self._env.observation_shape(params)
-        if isinstance(base_shape, dict):
-            assert "img" in base_shape
-            obs_shape = {**base_shape, **{"direction": 4}}
-        else:
-            obs_shape = {
-                "img": self._env.observation_shape(params),
-                "direction": 4,
-            }
+        obs_shape = extend_obs(base_shape, direction=4)
         return obs_shape
 
     def __extend_obs(self, timestep):
         direction = jax.nn.one_hot(timestep.state.agent.direction, num_classes=4)
-        if isinstance(timestep.observation, dict):
-            assert "img" in timestep.observation
-            extended_obs = {
-                **timestep.observation,
-                **{"direction": direction},
-            }
-        else:
-            extended_obs = {
-                "img": timestep.observation,
-                "direction": direction,
-            }
-
-        timestep = timestep.replace(observation=extended_obs)
-        return timestep
+        extended_obs = extend_obs(timestep.observation, direction=direction)
+        return timestep.replace(observation=extended_obs)
 
     def reset(self, params, key):
         timestep = self._env.reset(params, key)
@@ -120,44 +103,16 @@ class DirectionObservationWrapper(Wrapper):
 class RulesAndGoalsObservationWrapper(Wrapper):
     def observation_shape(self, params):
         base_shape = self._env.observation_shape(params)
-        if isinstance(base_shape, dict):
-            assert "img" in base_shape
-            obs_shape = {
-                **base_shape,
-                **{
-                    "goal_encoding": params.ruleset.goal.shape,
-                    "rule_encoding": params.ruleset.rules.shape,
-                },
-            }
-        else:
-            obs_shape = {
-                "img": self._env.observation_shape(params),
-                "goal_encoding": params.ruleset.goal.shape,
-                "rule_encoding": params.ruleset.rules.shape,
-            }
+        obs_shape = extend_obs(
+            base_shape, goal_encoding=params.ruleset.goal.shape, rule_encoding=params.ruleset.rules.shape
+        )
         return obs_shape
 
     def __extend_obs(self, timestep):
         goal_encoding = timestep.state.goal_encoding
         rule_encoding = timestep.state.rule_encoding
-        if isinstance(timestep.observation, dict):
-            assert "img" in timestep.observation
-            extended_obs = {
-                **timestep.observation,
-                **{
-                    "goal_encoding": goal_encoding,
-                    "rule_encoding": rule_encoding,
-                },
-            }
-        else:
-            extended_obs = {
-                "img": timestep.observation,
-                "goal_encoding": goal_encoding,
-                "rule_encoding": rule_encoding,
-            }
-
-        timestep = timestep.replace(observation=extended_obs)
-        return timestep
+        extended_obs = extend_obs(timestep.observation, goal_encoding=goal_encoding, rule_encoding=rule_encoding)
+        return timestep.replace(observation=extended_obs)
 
     def reset(self, params, key):
         timestep = self._env.reset(params, key)
@@ -168,3 +123,65 @@ class RulesAndGoalsObservationWrapper(Wrapper):
         timestep = self._env.step(params, timestep, action)
         timestep = self.__extend_obs(timestep)
         return timestep
+
+
+class GoalObservationWrapper(Wrapper):
+    def observation_shape(self, params) -> dict[str, tuple[int, ...]]:
+        base_shape = self._env.observation_shape(params)
+        return extend_obs(base_shape, goal_encoding=MAX_GOAL_ENCODING_LEN)
+
+    def __extend_obs(self, timestep) -> TimeStep:
+        extended_obs = extend_obs(timestep.observation, goal_encoding=timestep.state.goal_encoding)
+        return timestep.replace(observation=extended_obs)
+
+    def reset(self, params, key) -> TimeStep:
+        timestep = self._env.reset(params, key)
+        return self.__extend_obs(timestep)
+
+    def step(self, params, timestep, action) -> TimeStep:
+        timestep = self._env.step(params, timestep, action)
+        return self.__extend_obs(timestep)
+
+
+class PrevActionWrapper(Wrapper[EnvParamsT, tuple[EnvCarryT, Array]]):
+    """Put the previous action into the observation."""
+
+    def observation_shape(self, params) -> dict[str, tuple[int, ...]]:
+        shape = self._env.observation_shape(params)
+        return extend_obs(shape, prev_action=self._env.num_actions(params), reward=(), done=())
+
+    def __extend_timestep(self, timestep: TimeStep, prev_action, action) -> TimeStep:
+        extended_state = timestep.state.replace(carry=(timestep.state.carry, action))
+        obs = extend_obs(
+            timestep.observation,
+            prev_action=prev_action,
+            reward=timestep.reward,
+            done=timestep.last(),
+        )
+        return timestep.replace(state=extended_state, observation=obs)
+
+    def reset(self, params, key) -> TimeStep:
+        timestep = self._env.reset(params, key)
+        zero_action = jnp.asarray(0, dtype=jnp.uint8)
+        return self.__extend_timestep(timestep, zero_action, zero_action)
+
+    def step(self, params, timestep, action) -> TimeStep:
+        base_carry, prev_action = timestep.state.carry
+
+        # base environment step
+        base_state = timestep.state.replace(carry=base_carry)
+        timestep = timestep.replace(state=base_state)
+        timestep = self._env.step(params, timestep, action)
+
+        # add taken action to carry
+        action = jnp.asarray(action, dtype=jnp.uint8)
+        return self.__extend_timestep(timestep, prev_action, action)
+
+
+def extend_obs(base, **kwargs):
+    if isinstance(base, dict):
+        assert "img" in base, base
+        out = {**base, **kwargs}
+    else:
+        out = {"img": base, **kwargs}
+    return out
